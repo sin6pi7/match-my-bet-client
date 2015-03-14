e9ed206d91c2003d05f534cb3e67c25d62ff93b9
sin6pi7 <jac.zyla@gmail.com>
2015-03-14 12:39:21 +0100

    Judging works

--- ionic-app/www/js/controllers.js
+++ ionic-app/www/js/controllers.js
@@ -8,17 +8,44 @@ angular.module('starter.controllers',[])
     }
 }])
 .controller('JudgeController',
-	['$scope', 'USER', 'SOCKET_URL', '$state',function($scope, USER, SOCKET_URL, $state){
-    var socket = io(SOCKET_URL);
-    $scope.auctions = null;
+	['$scope', 'USER', 'SOCKET_URL', '$state', '$ionicLoading', function($scope, USER, SOCKET_URL, $state, $ionicLoading){
+    var socket = io(SOCKET_URL),
+        index = 0,
+        result = 0;
+
+    $ionicLoading.show({
+        template: 'Loading...'
+    });
+
+    $scope.auction = {
+        title: '',
+        image: ''
+    };
 
     socket.on('paired', function (data) {
     	if (data.auctions.length > 0) {
             $scope.$apply(function() {
                 $scope.auctions = data.auctions;
+                $scope.auction = $scope.auctions[index];
             });
-        } 
+            $ionicLoading.hide();
+        }
     });
+
+    $scope.nextAuction = function(btn) {
+        result += countResult(btn, index);
+        if (index === ($scope.auctions.length - 1)) {
+            socket.emit('results', result);
+            socket.disconnect();
+            return;
+        }
+        $scope.auction = $scope.auctions[++index];
+    }
+
+    var countResult = function(btn, index) {
+        var yes = (btn === 'YES') ? 1 : 0;
+        return Math.pow(2, index) * yes;
+    }
 
     socket.emit('join', USER.name);
 }]);
--- ionic-app/www/js/services.js
+++ ionic-app/www/js/services.js
@@ -1,3 +1,3 @@
 angular.module('starter.services',[])
 	.value('USER',{})
-	.value('SOCKET_URL','http://10.0.2.58:8000');
+	.value('SOCKET_URL','http://10.0.2.180:8000');
