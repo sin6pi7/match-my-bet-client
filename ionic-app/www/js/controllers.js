87807b7557b8630b0e04a73a7a4cc86f6f68b353
sin6pi7 <jac.zyla@gmail.com>
2015-03-14 16:01:48 +0100

    Styles finishing

--- ionic-app/www/js/controllers.js
+++ ionic-app/www/js/controllers.js
@@ -4,32 +4,35 @@ angular.module('starter.controllers',[])
     $scope.user = {};
 
     $scope.start = function() {
-        openFB.login(
-            function(response) {
-                if (response.status === 'connected') {
-                    console.log('Facebook login succeeded');
+        USER.name = $scope.user.name;
+        USER.image = "http://www.digibuzzme.com/wp-content/uploads/2012/11/error-404-road-not-found.jpg";
+        $state.go('judge');
+        // openFB.login(
+        //     function(response) {
+        //         if (response.status === 'connected') {
+        //             console.log('Facebook login succeeded');
 
-                        openFB.api({
-                            path: '/me',
-                            params: {fields: 'id,name'},
-                            success: function(user) {
-                                $scope.$apply(function() {
-                                    $scope.user = user;
-                                    USER = $scope.user;
-                                    USER.image = 'http://graph.facebook.com/' + USER.id + '/picture?width=270&height=270';
-                                    console.log(USER);
-                                    $state.go('judge');
-                                });
-                            },
-                            error: function(error) {
-                                $state.go('home');
-                            }
-                        });
-                } else {
-                    $state.go('home');
-                }
-            },
-            {scope: 'email,publish_actions'});
+        //                 openFB.api({
+        //                     path: '/me',
+        //                     params: {fields: 'id,name'},
+        //                     success: function(user) {
+        //                         $scope.$apply(function() {
+        //                             $scope.user = user;
+        //                             USER = $scope.user;
+        //                             USER.image = 'http://graph.facebook.com/' + USER.id + '/picture?width=270&height=270';
+        //                             console.log(USER);
+        //                             $state.go('judge');
+        //                         });
+        //                     },
+        //                     error: function(error) {
+        //                         $state.go('home');
+        //                     }
+        //                 });
+        //         } else {
+        //             $state.go('home');
+        //         }
+        //     },
+        //     {scope: 'email,publish_actions'});
     };
 }])
 .controller('JudgeController',
